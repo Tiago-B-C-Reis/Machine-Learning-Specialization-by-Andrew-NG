27004815190983950a5893094a3e8b976dfa8abe
Tiago-B-C-Reis <tiago.b.c.reis@hotmail.com>
2022-10-09 15:35:47 +0100

    stage_09-10-2022

--- multi-feature_predict(quantitative).py
+++ multi-feature_predict(quantitative).py
@@ -7,17 +7,47 @@ np.set_printoptions(precision=2)  # reduced display precision on numpy arrays
 
 X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
 y_train = np.array([460, 232, 178])
-print(X_train)
-print(f'\n{X_train.shape}')
-# data is stored in numpy array/matrix
-print(f"X Shape: {X_train.shape}, X Type:{type(X_train)})")
-print(X_train)
-print(f"y Shape: {y_train.shape}, y Type:{type(y_train)})")
-print(y_train)
 
-b_init = 785.1811367994083
-w_init = np.array([0.39133535, 18.75376741, -53.36032453, -26.42131618])
-print(f"w_init shape: {w_init.shape}, b_init type: {type(b_init)}")
+# Applying feature engineering + polynomial regression
+X_train2 = np.c_[X_train**2]
+X_super_train = np.concatenate((X_train, X_train2), axis=0)
+y_super_train = np.concatenate((y_train, y_train**2), axis=0)
+
+# data is stored in numpy array/matrix
+print(f'X_train array: \n{X_super_train}')
+print(f"X Shape: {X_super_train.shape}, X Type:{type(X_super_train)})\n")
+print(f'y_train array: \n{y_super_train}')
+print(f"y Shape: {y_super_train.shape}, y Type:{type(y_super_train)})\n")
+
+
+def zscore_normalize_features(X):
+    """
+    computes  X, zcore normalized by column
+
+    Args:
+      X (ndarray (m,n))     : input data, m examples, n features
+
+    Returns:
+      X_norm (ndarray (m,n)): input normalized by column
+      mu (ndarray (n,))     : mean of each feature
+      sigma (ndarray (n,))  : standard deviation of each feature
+    """
+    # find the mean of each column/feature
+    mu = np.mean(X, axis=0)  # mu will have shape (n,)
+    # find the standard deviation of each column/feature
+    sigma = np.std(X, axis=0)  # sigma will have shape (n,)
+    # element-wise, subtract mu for that column from each example, divide by std for that column
+    X_norm = (X - mu) / sigma
+
+    return X_norm, mu, sigma
+
+
+# normalize the original features | --> can use X_train or X_super_train
+X_norm, X_mu, X_sigma = zscore_normalize_features(X_super_train)
+
+print(f"X_mu = {X_mu}, \nX_sigma = {X_sigma}")
+print(f"Peak to Peak range by column in Raw        X:{np.ptp(X_super_train,axis=0)}")
+print(f"Peak to Peak range by column in Normalized X:{np.ptp(X_norm,axis=0)}")
 
 
 def predict_single_loop(x, w, b):
@@ -130,14 +160,16 @@ def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, 
     w = copy.deepcopy(w_in)  # avoid modifying global w within function
     b = b_in
 
+    print("Iteration|    Cost    |    w0    |    w1    |    w2    |    w3    |"
+          "    b    |    djdw0    |    djdw1    |    djdw2    |    djdw3    |    djdb   ")
     for i in range(num_iters):
 
         # Calculate the gradient and update the parameters
-        dj_db, dj_dw = gradient_function(X, y, w, b)  ##None
+        dj_db, dj_dw = gradient_function(X, y, w, b)  # None
 
         # Update Parameters using w, b, alpha and gradient
-        w = w - alpha * dj_dw  ##None
-        b = b - alpha * dj_db  ##None
+        w = w - alpha * dj_dw  # None
+        b = b - alpha * dj_db  # None
 
         # Save cost J at each iteration
         if i < 100000:  # prevent resource exhaustion
@@ -145,24 +177,26 @@ def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, 
 
         # Print cost every at intervals 10 times or as many iterations if < 10
         if i % math.ceil(num_iters / 10) == 0:
-            print(f"Iteration {i:4d}: Cost {J_history[-1]:8.2f}   ")
+            print(f" {i:7d}:{J_history[-1]:10.2f} {w[0]:10.2f} {w[1]:10.2f} {w[2]:10.2f} {w[3]:10.2f} {b:10.2f} "
+                  f"{dj_dw[0]:12.2f} {dj_dw[1]:13.2f} {dj_dw[2]:13.2f} {dj_dw[3]:13.2f} {dj_db:12.2f} ")
 
     return w, b, J_history  # return final w,b and J history for graphing
 
 
 # initialize parameters
-initial_w = np.zeros_like(w_init)
+initial_w = np.zeros(X_train.shape[1])
 initial_b = 0.
 # some gradient descent settings
-iterations = 1000
-alpha = 5.0e-7
+iterations = 10000
+alpha = 8.3e-7
 
-# run gradient descent
-w_final, b_final, J_hist = gradient_descent(X_train, y_train, initial_w, initial_b,
+# run gradient descent | --> Can use X_train or X_norm, to use x_norm it's better to have a big data set
+w_final, b_final, J_hist = gradient_descent(X_super_train, y_train, initial_w, initial_b,
                                             compute_cost, compute_gradient,
                                             alpha, iterations)
 
-print(f"b,w found by gradient descent: {b_final:0.2f},{w_final} ")
+print(f"\n--> b = {b_final:0.2f} | w found by gradient descent = {w_final}\n ")
+
 m, _ = X_train.shape
 for i in range(m):
-    print(f"prediction: {np.dot(X_train[i], w_final) + b_final:0.2f}, target value: {y_train[i]}")
+    print(f"Prediction: {np.dot(X_train[i], w_final) + b_final:0.2f}, target value: {y_train[i]}")
