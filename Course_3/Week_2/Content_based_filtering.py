1d155f17c594d88b06aa79f7611c53921f48e83f
Tiago-B-C-Reis <tiago.b.c.reis@hotmail.com>
2022-11-18 11:32:55 +0000

    Stage_18-11-2022

---
+++ Course_3/Week_2/Content_based_filtering.py
@@ -0,0 +1,47 @@
+# We will use familiar packages, NumPy, TensorFlow and helpful routines from scikit-learn.
+# We will also use tabulate to neatly print tables and Pandas to organize tabular data.
+
+import numpy as np
+import numpy.ma as ma
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.model_selection import train_test_split
+# import tabulate
+pd.set_option("display.precision", 1)
+
+top10_df = pd.read_csv("./Content_based_filtering_data/movies.csv")
+
+# ---------------------------------------------------------------------------------------------------
+# scale training data
+item_train_unscaled = item_train
+user_train_unscaled = user_train
+y_train_unscaled = y_train
+
+scalerItem = StandardScaler()
+scalerItem.fit(item_train)
+item_train = scalerItem.transform(item_train)
+
+scalerUser = StandardScaler()
+scalerUser.fit(user_train)
+user_train = scalerUser.transform(user_train)
+
+scalerTarget = MinMaxScaler((-1, 1))
+scalerTarget.fit(y_train.reshape(-1, 1))
+y_train = scalerTarget.transform(y_train.reshape(-1, 1))
+# ynorm_test = scalerTarget.transform(y_test.reshape(-1, 1))
+
+print(np.allclose(item_train_unscaled, scalerItem.inverse_transform(item_train)))
+print(np.allclose(user_train_unscaled, scalerUser.inverse_transform(user_train)))
+# ---------------------------------------------------------------------------------------------------
+
+# Train_Set, Test_set split -------------------------------------------------------------------------
+item_train, item_test = train_test_split(item_train, train_size=0.80, shuffle=True, random_state=1)
+user_train, user_test = train_test_split(user_train, train_size=0.80, shuffle=True, random_state=1)
+y_train, y_test = train_test_split(y_train,    train_size=0.80, shuffle=True, random_state=1)
+print(f"movie/item training data shape: {item_train.shape}")
+print(f"movie/item test data shape: {item_test.shape}")
+# ---------------------------------------------------------------------------------------------------
+
+
